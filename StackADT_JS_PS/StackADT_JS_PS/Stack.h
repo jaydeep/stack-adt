ca8a36eebe5a4dabf2b35cde62eb0728e631a90b
jaydeep <jdsingh94@gmail.com>
2016-10-26 13:50:28 -0700

    added stack skeleton

---
+++ StackADT_JS_PS/StackADT_JS_PS/Stack.h
@@ -0,0 +1,16 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "SinglyLinkedList.h"
+
+template <class T>
+class Stack : protected SinglyLinkedList <T>
+{
+protected:
+
+public:
+	//Constructors
+
+	//Behaviors
+};
+#endif
